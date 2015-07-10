62c8bf5c94fe47636beac6f44eb22e248b24e50f
Bogdan Boamfa <bogdan.boamfa@gmail.com>
2015-07-11 00:32:38 +0300

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workoutClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the workoutClientApp
+ */
+angular.module('workoutClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workoutClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the workoutClientApp
+ */
+angular.module('workoutClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
