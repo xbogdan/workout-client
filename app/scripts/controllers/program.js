7e9d939e9df9868c67e29bd6fdc8a9e840c2e3f4
Bogdan Boamfa <bogdan.boamfa@gmail.com>
2015-07-23 23:03:05 +0300

    Add update after refresh.

--- app/scripts/controllers/program.js
+++ app/scripts/controllers/program.js
@@ -12,6 +12,7 @@ angular.module('workoutClientApp')
     // Action for the login form
     $scope.toggleEdit = toggleEdit;
     $scope.submit = submit;
+    $scope.toggleDestroyElement = toggleDestroyElement;
     $scope.treeOptions = {
       accept: function(sourceNode, destNodes, destIndex) {
         var sourceType = sourceNode.$element.attr('data-type');
@@ -22,8 +23,7 @@ angular.module('workoutClientApp')
         e.source.nodeScope.$modelValue.ord = e.dest.index
         var nodes = e.dest.nodesScope.childNodes();
         for (var i = 0; i < nodes.length; i++) {
-          var c = nodes[i];
-          c.$modelValue.ord = i;
+          nodes[i].$modelValue.ord = i;
         }
       }
     };
@@ -32,15 +32,22 @@ angular.module('workoutClientApp')
       $scope.showEditButton = true;
       programs.Program($routeParams.id, function(data) {
         $scope.program = data.program;
-        for (var i = 0; i < $scope.program.program_days_attributes.length; i++) {
-          $scope.program.program_days_attributes[i].type = 'ceva';
-        }
       });
     } else {
       $scope.editing = true;
       $scope.showEditButton = false;
       $scope.master = {};
     }
+
+    function toggleDestroyElement(element, event) {
+      if (typeof element._destroy === 'undefined' || element._destroy === false) {
+        $(event.target).html('cancel').closest('li').addClass('destroy');
+        element._destroy = true;
+      } else if (element._destroy === true) {
+        $(event.target).html('remove').closest('li').removeClass('destroy');
+        element._destroy = false;
+      }
+    };
 
     function toggleEdit(updated) {
       $scope.editing = !$scope.editing;
@@ -50,11 +57,15 @@ angular.module('workoutClientApp')
         $scope.master = angular.copy($scope.masterCopy);
       }
     };
+
     function submit() {
       if ($routeParams.id) {
         programs.editProgram($scope.master, function(data) {
-          $scope.program = $scope.master;
           $scope.toggleEdit(true);
+          programs.Program($routeParams.id, function(data) {
+            $scope.program = data.program;
+          });
+          $scope.program = $scope.master;
         });
       } else {
         // programs
